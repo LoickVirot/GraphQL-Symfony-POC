8d4fe051522a69fba024781f1c27cabc2aa727df
Loïck VIROT <l.virot@jeuneafrique.com>
2020-01-02 15:58:21 +0100

    Get movies list in home with graphql

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,36 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        // Get data
+        $url = 'https://testgraphql.local';
+
+        $query = [
+            "query" => "{
+                movies {
+                id
+                title
+                resume
+                }
+            }",
+        ];
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($query));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+        ));
+        // DEVELOP ONLY
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        // END DEVELOP ONLY
+        $result = json_decode(curl_exec($ch));
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'movies' => $result->data->movies
         ]);
     }
 }
